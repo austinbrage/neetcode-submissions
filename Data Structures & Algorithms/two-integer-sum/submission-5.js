fc3e940b2577fe126dfdf2ca1517587f25f29764
Austin Brage <135470816+austinbrage@users.noreply.github.com>
2026-09-01 09:55:46 -0300

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = new Map()
+
+        for (let i = 0; i < nums.length; i++) {
+            const current = nums[i]
+            const complement = target - current
+
+            if(seen.has(complement)) {
+                const value = seen.get(complement)
+                return [value, i]
+            }
+
+            seen.set(current, i)
+        }
+
+        return []
+    }
+}
