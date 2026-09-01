848d9f0adb393102eb453a4f86fe0775363e01a2
Austin Brage <135470816+austinbrage@users.noreply.github.com>
2026-09-01 08:47:55 -0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seen = new Set()
+
+        for(let i = 0; i < nums.length; i++) {
+            const current = nums[i]
+
+            if(seen.has(current)) {
+                return true
+            } else {
+                seen.add(current)
+            }
+        }
+
+        return false
+    }
+}
